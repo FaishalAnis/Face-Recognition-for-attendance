28ab8cb49e3f0b20faae7131d57367ae7ffbc0d1
MD FAISHAL ANIS <137764041+FaishalAnis@users.noreply.github.com>
2024-10-23 10:11:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 import csv
 from datetime import datetime
 
+
 video_capture = cv2.VideoCapture(0)
 #Load known faces
 faishals_image = face_recognition.load_image_file("faces/Faishal.jpg")
